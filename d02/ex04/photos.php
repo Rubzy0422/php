fa0bfa213e891a8950747fca3a13871fa76b9c7a
Ruben COETZER <rcoetzer@c4r21s4.wethinkcode.co.za>
2019-10-09 14:42:09 +0200

    fixed some stuff a bit

--- d02/ex04/photos.php
+++ d02/ex04/photos.php
@@ -1,18 +1,24 @@
 #!/usr/bin/php
 <?php
-
-	if ($argv > 1)
+	if ($argc > 1)
 	{
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $argv[1]);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		$output = curl_exec($ch);
-		curl_close($ch);
-		echo $output;
-	//	"<a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1"
-		preg_match_all("/(<img src=\".*\")/i", $file);
-		foreach ($file as $img)
-			echo $img;
-	}
+		$html = file_get_contents($argv[1]);
+		preg_match_all("<img.+src=\"(.+?)\".+>", $html, $matches);
+		$dir = parse_url($argv[1], PHP_URL_HOST);
+		if (!file_exists($dir))
+			mkdir($dir);
 
+		foreach ($matches[1] as $img_url)
+		{
+			$url = parse_url($img_url);
+			if (sizeof($url) == 1)
+				$img = $argv[1] . $url["path"];
+			else 
+				$img = $img_url;
+			$img_arr = explode("/", $img);
+			$img_dir = $dir . "/" . end($img_arr);
+
+			file_put_contents($img_dir, file_get_contents($img));
+		}
+}
 ?>
