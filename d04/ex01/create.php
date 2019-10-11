657da7280dd90cd28bc0f7d4a949b32f0cbf3e1a
Ruben COETZER <rcoetzer@c4r21s4.wethinkcode.co.za>
2019-10-11 06:36:54 +0200

    I think this will work ex01

--- d04/ex01/create.php
+++ d04/ex01/create.php
@@ -1,29 +1,26 @@
 <?php
-	if (!$_POST['login'] && !$_POST['passwd'] && $_POST['submit'] && $_POST['submit'] === "OK"){
+	if (!$_POST['login'] && !$_POST['passwd'] && $_POST['submit'] && $_POST['submit'] === "OK") {
 		echo "ERROR\n";
 		exit();
 	}
-	$file = file_get_contents('./private/passwd');
-	if (!file_exists("private/")){
+	if (!file_exists("private/")) {
 		mkdir("./private");
-		if (!file_exists("./private/passwd")){
+		if (!file_exists("./private/passwd")) {
 			file_put_contents("./private/passwd", "");
 		}
 	}
-	// else {
-	// 	$users = unserialize(file_get_contents('../private/passwd'));
-	// 	if ($users){
-	// 		$exist = 0;
-	// 		foreach ($users as $user => $v){
-	// 			if ($v['login'] === $_POST['login'])
-	// 				$exist = 1;
-	// 			if ($exist){
-	// 				echo "ERROR\n";
-	// 				exit();
-	// 			}
-	// 		}
-	// 	}														SOMEONE FIX REUBENS CODE
-		$data = serialize(array("login", $_POST['login'], "passwd", hash("sha512", $_POST['passwd'])));
-		file_put_contents("./private/passwd", $file . $data . PHP_EOL);
+	else { //file exists get content
+		$users = unserialize(file_get_contents('../private/passwd'));
+		if ($users) {
+			foreach ($users as $user => $tmp) {
+				if ($tmp['login'] === $_POST['login'])
+				{
+					echo "ERROR\n";
+					exit();
+				}
+			}
+		}
 	}
+	$data = serialize(array("login", $_POST['login'], "passwd", hash("sha512", $_POST['passwd'])));
+	file_put_contents("./private/passwd", $file . $data . PHP_EOL);
 ?>
