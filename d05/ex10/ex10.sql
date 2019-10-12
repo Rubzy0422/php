a77811ec20e3f1929385ea250a3239c42c0ab8dd
Ruben COETZER <rcoetzer@c4r21s4.wethinkcode.co.za>
2019-10-12 07:55:11 +0200

    fixed d05 ex10

--- d05/ex10/ex10.sql
+++ d05/ex10/ex10.sql
@@ -1,5 +1,8 @@
 USE db_rcoetzer;
 
-SELECT title AS Title, summary AS Summary, prod_year FROM film
-WHERE id_genre = 'erotic'
-ORDER BY prod_year DESC;
+SELECT `title` AS `Title`, `summary` AS `Summary`, `prod_year`
+FROM `film`
+INNER JOIN `genre`
+ON `film`.id_genre = `genre`.id_genre
+WHERE `genre`.name = 'erotic'
+ORDER BY `prod_year` DESC
